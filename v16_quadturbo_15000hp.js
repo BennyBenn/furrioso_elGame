e0e2223890e3d2b75a2c82378a5246fa14a7bb97
kevinkalvin62 <134903820+kevinkalvin62@users.noreply.github.com>
2024-09-16 22:23:27 -0600

    imagenes aleatorias
    cambiar ruta de imagenes en loj.js

--- v16_quadturbo_15000hp.js
+++ v16_quadturbo_15000hp.js
@@ -1,17 +1,11 @@
-const $_cuerpo_tabla = document.querySelector("#_cuerpo_tabla");
-let salidas=[];
-
-function display_text(){
-    const $tr = document.createElement("tr");
-    let $tTexto=document.createElement("img");
-    $tTexto.src=("img/_quest/texto1/texto1.png")
-    $tTexto.style.width="100%";
-    $tTexto.style.height="100%";
-    $_cuerpo_tabla.appendChild($tTexto); 
-}
-
-function display_qa(){
-    while ($_cuerpo_tabla.firstChild) {
-        $_cuerpo_tabla.removeChild($_cuerpo_tabla.firstChild);
-    }
-}
+function getRandomImage() {
+    const imagesFolder = 'img/_backgrounds/';
+    const numImages = 10; 
+    const randomIndex = Math.floor(Math.random() * numImages) + 1; 
+    const randomImage = imagesFolder + randomIndex + '.png'; 
+  
+    document.body.style.backgroundImage = `url(${randomImage})`; 
+  }
+  
+  
+  window.onload = getRandomImage;
